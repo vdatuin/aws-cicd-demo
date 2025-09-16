6b0ede22a1650ce378700ed4a15d0abd320326b6
vdatuin <131155737+vdatuin@users.noreply.github.com>
2025-09-16 16:50:50 +0800

    Delete db/changelog/bootstrap/R-000-roles.sql

--- db/changelog/bootstrap/R-000-roles.sql
+++
@@ -1,8 +0,0 @@
---changeset bootstrap:ensure-app-readonly labels:bootstrap runInTransaction:true
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'app_readonly') THEN
-    CREATE ROLE app_readonly NOLOGIN;
-  END IF;
-END
-$$;
